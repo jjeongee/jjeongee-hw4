7c1ab561e0d657527e9bfb3f2b86bbf3491fe093
jjeongee <starstar117@naver.com>
2023-05-08 15:36:38 +0900

    jjeongee hw4

---
+++ aboutme.js
@@ -0,0 +1,13 @@
+function anchtime(status){
+    document.getElementById('anchtime').style.display=status;
+}
+document.getElementById('close').addEventListener('click',function(){
+    document.getElementById('anchtime').style.display=none;
+});
+
+function harrypotter(status){
+    document.getElementById('harrypotter').style.display=status;
+}
+document.getElementById('close').addEventListener('click',function(){
+    document.getElementById('harrypotter').style.display=none;
+});
